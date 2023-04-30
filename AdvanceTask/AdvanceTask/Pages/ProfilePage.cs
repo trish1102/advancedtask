dec4f4ab31695abe754723c84a36e82b2aa183dc
trupti <truptisharma105@gmail.com>
2023-05-01 10:47:26 +1200

    testcases registration and profile

---
+++ AdvanceTask/AdvanceTask/Pages/ProfilePage.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+namespace AdvanceTask.Pages
+{
+    public class ProfilePage : Commondriver
+    {
+       public IWebElement shareskillButton => driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[1]/div/div[2]/a"));
+       public IWebElement managelistingButton => driver.FindElement(By.XPath("//a[contains(text(),'Manage Listings')]"));
+
+        public void ShareSkill()
+        {
+            //click on Shareskill
+            Wait.TurnOnWait();
+            shareskillButton.Click();
+
+        }
+        public void ManageListing()
+        {
+            //click on ManageListing
+            
+            Wait.WaitForElementToBeClickable(driver, "XPath", "//a[contains(text(),'Manage Listings')]", 10);
+            managelistingButton.Click();
+        }
+    }
+}
---
+++ AdvanceTask/AdvanceTask/Test/ShareSkillTest.cs
@@ -0,0 +1,68 @@
+﻿
+
+
+namespace AdvanceTask.Test
+{
+    [TestFixture]
+    public class ShareSkillTest:Commondriver
+    {
+        SignIn singninObj = new SignIn();
+        ProfilePage profilepageObj = new ProfilePage();
+        ShareSkill skillObj = new ShareSkill();
+        ManageListing managelistingObj = new ManageListing();
+       
+
+
+        [Test,Order(1)]
+        public void CreateSkillListing()
+        {
+           
+            test1 = extent.CreateTest("CreateSkillListing");
+            test1.Log(Status.Info, "test started");
+            singninObj.SignInAction();
+            test1.Log(Status.Info, "SignIn successfully");
+            profilepageObj.ShareSkill();
+            
+            try
+            {
+                Wait.TurnOnWait();
+                test1.Log(Status.Pass, "successfully clicked on ShareSkill link");
+            }
+            catch(Exception e)
+            {
+                test1.Log(Status.Fail, "Fail to click on ShareSkill link");
+                throw;
+
+            }
+            skillObj.CreateSkill();
+            test1.Log(Status.Info,"Listing Created successfully");
+
+
+            
+        }
+        
+        [Test,Order(2)]
+        public void UpdateSkillListing()
+        {
+            test1 = extent.CreateTest("Update SkillListing");
+            singninObj.SignInAction();
+            test1.Log(Status.Info, "SignIn successfully");
+            profilepageObj.ManageListing();
+            Wait.TurnOnWait();
+            test1.Log(Status.Pass, "Click on ManageListing successfully");
+            managelistingObj.UpdateRecord();
+            test1.Log(Status.Pass, "UpdateRecord function worked successfully");
+        }
+        [Test,Order(3)]
+        public void DeleteSkillListing()
+        {
+            test1 = extent.CreateTest("Delete SkillListing");
+            singninObj.SignInAction();
+            test1.Log(Status.Info, "SignIn successfully");
+            profilepageObj.ManageListing();
+            test1.Log(Status.Pass, "Click on ManageListing successfully");
+            managelistingObj.DeleteRecord();
+            test1.Log(Status.Pass, "Record Deleted successfully");
+        }
+    }
+}
